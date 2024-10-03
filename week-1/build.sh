cd20d81e329c078cd5d4833a559812e0fd000a70
niqta <niqta@vk.ru>
2024-10-03 15:59:51 +0300

    first commit

---
+++ week-1/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd task-1 && ./build.sh && cd -
+cd task-2 && ./build.sh && cd -
+cd task-3 && ./build.sh && cd -
---
+++ week-1/task-2/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf bin
+mkdir bin
+g++ -o bin/Z2 src/Z2.cpp
