b59427423c530955c16d406abaf87695f051b162
niqta <niqta@vk.ru>
2024-10-03 14:46:34 +0300

    Revert "first commit"

--- week-2/task-3/task3-2/src/Passenger.cpp
+++
@@ -1,8 +0,0 @@
-#include "Passenger.h"
-
-Passenger::Passenger(const std::string& n) : name(n) {} // Реализация конструктора
-
-std::string Passenger::getName() const {
-    return name; // Возврат имени
-}
-
