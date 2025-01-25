6c2b9b8d0d6b23b24a79a5b28cf375e67f222647
niqta <niqta@vk.ru>
2025-01-25 23:34:16 +0300

    first commit

---
+++ week-12 /task2/build/CMakeFiles/PasswordCracker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PasswordCracker.dir/src/main.cpp.o"
+  "CMakeFiles/PasswordCracker.dir/src/main.cpp.o.d"
+  "CMakeFiles/PasswordCracker.dir/src/password_cracker.cpp.o"
+  "CMakeFiles/PasswordCracker.dir/src/password_cracker.cpp.o.d"
+  "PasswordCracker"
+  "PasswordCracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PasswordCracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
