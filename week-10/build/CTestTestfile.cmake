855b6899dfacacc51d91e7666d0622c18b888a5d
niqta <niqta@vk.ru>
2025-01-25 20:33:59 +0300

    first commit

---
+++ week-10/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/niqta/Study/week-10
+# Build directory: /home/niqta/Study/week-10/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/niqta/Study/week-10/build/stack_test[1]_include.cmake")
+subdirs("_deps/googletest-build")
---
+++ week-10/build/stack_test[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/niqta/Study/week-10/build/stack_test[1]_tests.cmake")
+  include("/home/niqta/Study/week-10/build/stack_test[1]_tests.cmake")
+else()
+  add_test(stack_test_NOT_BUILT stack_test_NOT_BUILT)
+endif()
